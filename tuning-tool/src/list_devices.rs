e21e30ddcf1d359992986353e30df481fd33b851
Richard Cook <rcook@rcook.org>
2024-10-02 18:36:03 -0700

    Display message if there are no MIDI output ports

--- tuning-tool/src/list_devices.rs
+++ tuning-tool/src/list_devices.rs
@@ -3,14 +3,18 @@ use anyhow::{anyhow, Result};
 
 pub(crate) fn list_devices() -> Result<()> {
     let midi_output = make_midi_output()?;
-    let mut names = midi_output
-        .ports()
-        .iter()
-        .map(|p| midi_output.port_name(&p).map_err(|e| anyhow!(e)))
-        .collect::<Result<Vec<_>>>()?;
-    names.sort();
-    for name in names {
-        println!("{}", name);
+    if midi_output.port_count() == 0 {
+        println!("You have no MIDI output ports");
+    } else {
+        let mut names = midi_output
+            .ports()
+            .iter()
+            .map(|p| midi_output.port_name(&p).map_err(|e| anyhow!(e)))
+            .collect::<Result<Vec<_>>>()?;
+        names.sort();
+        for name in names {
+            println!("{}", name);
+        }
     }
     Ok(())
 }
