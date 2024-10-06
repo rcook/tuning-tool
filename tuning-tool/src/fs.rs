3af37c960198441a40c8bc15a8e891823c1a004a
Richard Cook <rcook@rcook.org>
2024-10-06 12:50:08 -0700

    Generalize method to AsRef<Path>

--- tuning-tool/src/fs.rs
+++ tuning-tool/src/fs.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-pub(crate) fn read_to_string_lossy(path: &Path) -> Result<String> {
+pub(crate) fn read_to_string_lossy<P: AsRef<Path>>(path: P) -> Result<String> {
     let mut file = File::open(path)?;
     let mut buffer = vec![];
     file.read_to_end(&mut buffer)?;
