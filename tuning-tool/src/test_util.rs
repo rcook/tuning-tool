72c6f45874a28b1446e5185fb87ea0a49374ddae
Richard Cook <rcook@rcook.org>
2024-10-07 09:59:14 -0700

    Move expected frequencies into separate files

--- tuning-tool/src/test_util.rs
+++ tuning-tool/src/test_util.rs
@@ -6,6 +6,35 @@ use anyhow::{anyhow, Result};
 use std::path::Path;
 
 #[allow(unused)]
+pub(crate) fn read_expected_frequencies<P: AsRef<Path>>(path: P) -> Result<Vec<f64>> {
+    let path = path.as_ref();
+    let file = RESOURCE_DIR.get_file(path).ok_or_else(|| {
+        anyhow!(
+            "Expected frequency file {path} could not be opened",
+            path = path.display()
+        )
+    })?;
+    let s = file.contents_utf8().ok_or_else(|| {
+        anyhow!(
+            "Could not decode frequency file {path} as UTF-8",
+            path = path.display()
+        )
+    })?;
+
+    s.lines()
+        .filter_map(|line| {
+            let temp = line.trim();
+            if temp.is_empty() {
+                None
+            } else {
+                Some(temp)
+            }
+        })
+        .map(|line| line.parse::<f64>().map_err(|e| anyhow!(e)))
+        .collect::<Result<Vec<_>>>()
+}
+
+#[allow(unused)]
 pub(crate) fn read_test_syx_file<P: AsRef<Path>>(path: P) -> Result<Vec<u8>> {
     let path = path.as_ref();
     Ok(RESOURCE_DIR
